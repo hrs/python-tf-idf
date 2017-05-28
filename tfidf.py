bc0b0cc87527c94889dde30815ee0b0ac060d28e
Harry Schwartz <hello@harryrschwartz.com>
2017-05-28 10:28:30 -0700

    Conform to PEP8
    * Use 4 spaces of indentation instead of 2,
    * Use double-newlines between imports and class definitions, and
    * Wrap long lines to 80 characters.

--- tfidf.py
+++ tfidf.py
@@ -1,58 +1,66 @@
 #!/usr/bin/env python
 
-"""
-The simplest TF-IDF library imaginable.
+"""The simplest TF-IDF library imaginable.
 
-Add your documents as two-element lists `[docname, [list_of_words_in_the_document]]` with `addDocument(docname, list_of_words)`. Get a list of all the `[docname, similarity_score]` pairs relative to a document by calling `similarities([list_of_words])`.
+Add your documents as two-element lists `[docname,
+[list_of_words_in_the_document]]` with `addDocument(docname, list_of_words)`.
+Get a list of all the `[docname, similarity_score]` pairs relative to a
+document by calling `similarities([list_of_words])`.
 
 See the README for a usage example.
+
 """
 
 import sys
 import os
 
+
 class TfIdf:
-  def __init__(self):
-    self.weighted = False
-    self.documents = []
-    self.corpus_dict = {}
+    def __init__(self):
+        self.weighted = False
+        self.documents = []
+        self.corpus_dict = {}
 
-  def add_document(self, doc_name, list_of_words):
-    # building a dictionary
-    doc_dict = {}
-    for w in list_of_words:
-      doc_dict[w] = doc_dict.get(w, 0.) + 1.0
-      self.corpus_dict[w] = self.corpus_dict.get(w, 0.0) + 1.0
+    def add_document(self, doc_name, list_of_words):
+        # building a dictionary
+        doc_dict = {}
+        for w in list_of_words:
+            doc_dict[w] = doc_dict.get(w, 0.) + 1.0
+            self.corpus_dict[w] = self.corpus_dict.get(w, 0.0) + 1.0
 
-    # normalizing the dictionary
-    length = float(len(list_of_words))
-    for k in doc_dict:
-      doc_dict[k] = doc_dict[k] / length
+        # normalizing the dictionary
+        length = float(len(list_of_words))
+        for k in doc_dict:
+            doc_dict[k] = doc_dict[k] / length
 
-    # add the normalized document to the corpus
-    self.documents.append([doc_name, doc_dict])
+        # add the normalized document to the corpus
+        self.documents.append([doc_name, doc_dict])
 
-  def similarities(self, list_of_words):
-    """Returns a list of all the [docname, similarity_score] pairs relative to a list of words."""
+    def similarities(self, list_of_words):
+        """Returns a list of all the [docname, similarity_score] pairs relative to a
+list of words.
 
-    # building the query dictionary
-    query_dict = {}
-    for w in list_of_words:
-      query_dict[w] = query_dict.get(w, 0.0) + 1.0
+        """
 
-    # normalizing the query
-    length = float(len(list_of_words))
-    for k in query_dict:
-      query_dict[k] = query_dict[k] / length
+        # building the query dictionary
+        query_dict = {}
+        for w in list_of_words:
+            query_dict[w] = query_dict.get(w, 0.0) + 1.0
 
-    # computing the list of similarities
-    sims = []
-    for doc in self.documents:
-      score = 0.0
-      doc_dict = doc[1]
-      for k in query_dict:
-        if k in doc_dict:
-          score += (query_dict[k] / self.corpus_dict[k]) + (doc_dict[k] / self.corpus_dict[k])
-      sims.append([doc[0], score])
+        # normalizing the query
+        length = float(len(list_of_words))
+        for k in query_dict:
+            query_dict[k] = query_dict[k] / length
 
-    return sims
+        # computing the list of similarities
+        sims = []
+        for doc in self.documents:
+            score = 0.0
+            doc_dict = doc[1]
+            for k in query_dict:
+                if k in doc_dict:
+                    score += (query_dict[k] / self.corpus_dict[k]) + (
+                      doc_dict[k] / self.corpus_dict[k])
+            sims.append([doc[0], score])
+
+        return sims
